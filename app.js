fd72102dd902e7c68185f84a8a171d2851d59af2
kokodecontee <kokodecontee123@gmail.com>
2022-08-04 03:13:23 +1200

    initial server setup

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require ("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(5000)
