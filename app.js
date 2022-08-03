75a5c957fd888aeb049a257b0253f191ec7729f2
kokodecontee <kokodecontee123@gmail.com>
2022-08-04 03:23:24 +1200

    Add routes folder that contains api subfolder with channel, message and users js files

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require ("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-
+const users = require("./routes/api/users");
+const messages = require("./routes/api/messages");
+const channel = require("./routes/api/channel");
 const app = express();
+
+
 
 app.use(bodyParser.json());
 
