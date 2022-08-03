a8a04d5e112d6cf1638bc5bf99a4576f547d6481
kokodecontee <kokodecontee123@gmail.com>
2022-08-04 01:09:11 +1200

    Add mongodb database to project

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ const users = require("./routes/api/users");
 const messages = require("./routes/api/messages");
 const channel = require("./routes/api/channel");
 const app = express();
+// created DB
+mongoose
+.connect("mongodb://localhost:27017", {useNewUrlParser: true})
+.then(()=>{
+    app.listen(4000, ()=>{
+        console.log("server is listening on port 4000, mongodb connected")
+    }) 
+})
+.catch((err) => {
+    console.log(err)
+})
 
 
 
 app.use(bodyParser.json());
 
-app.listen(5000)
